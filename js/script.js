fce9376910319e828c7582007f4041284826f458
NazarShevtsov <68178685+NazarShevtsov@users.noreply.github.com>
2021-07-17 20:56:30 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,3 +8,34 @@ $(document).ready(function(){
 		$('.fa-search').toggleClass('move');
 	});
 });
+
+new Swiper('.members-slider', {
+
+	pagination: {
+		el: '.swiper-pagination',
+		type: 'bullets',
+		clickable: true,
+	},
+	
+	slidesPerView: 3,
+	loop: true,
+	loopedSlides: 3,
+	speed: 800,
+
+	autoplay: {
+		delay: 7000,
+		disableOnInteraction: false
+	},
+
+	breakpoints:{
+		320:{
+			slidesPerView: 1,
+		},
+		768:{
+			slidesPerView: 2,
+		},
+		1024:{
+			slidesPerView: 3,
+		},
+	},
+});
