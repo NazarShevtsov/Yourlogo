799fd1b51334c483a9ba3b0fecc5fe5bab1e312a
NazarShevtsov <68178685+NazarShevtsov@users.noreply.github.com>
2021-07-18 21:36:55 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+'use strict'
+
 $(document).ready(function(){
 	$('.header__burger').click(function(event){
 		$('.header__burger, .header__menu').toggleClass('active');
@@ -38,4 +40,54 @@ new Swiper('.members-slider', {
 			slidesPerView: 3,
 		},
 	},
+
+	preloadImages: false,
+
+	lazy: {
+		loadOnTransitionStart: true,
+		loadPrevNext: false,
+	},
+	watchSlidesProgress: true,
+	watchSlidesVisibility: true,
+
 });
+
+
+//lazy loading
+const lazyImages = document.querySelectorAll('img[data-src], source[data-srcset]');
+const windowHeight = document.documentElement.clientHeight;
+
+let lazyImagesPositions = [];
+if(lazyImages.length>0){
+	lazyImages.forEach(img => {
+		if(img.dataset.src || img.dataset.srcset){
+			lazyImagesPositions.push(img.getBoundingClientRect().top + pageYOffset);
+			lazyScrollCheck();
+		}
+	});
+}
+
+window.addEventListener('scroll', lazyScroll);
+
+function lazyScroll(){
+	if(document.querySelectorAll('img[data-src], source[data-srcset]').length > 0){
+		lazyScrollCheck();
+	}
+}
+
+function lazyScrollCheck(){
+	let imgIndex = lazyImagesPositions.findIndex(
+		item => pageYOffset > item - windowHeight
+	);
+	if(imgIndex >= 0){
+		if(lazyImages[imgIndex].dataset.src){
+			lazyImages[imgIndex].src = lazyImages[imgIndex].dataset.src;
+			lazyImages[imgIndex].removeAttribute('data-src');
+		} else if(lazyImages[imgIndex].dataset.srcset){
+			lazyImages[imgIndex].srcset = lazyImages[imgIndex].dataset.srcset;
+			lazyImages[imgIndex].removeAttribute('data-srcset');
+		}
+		delete lazyImagesPositions[imgIndex];
+	}
+}
+
